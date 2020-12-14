cb0c42d71efefee9a7d96b6d94ccc5c8f1f3c1fd
andreluizlq <andreluizlq1@gmail.com>
2020-12-14 10:48:37 -0300

    first commit

---
+++ Styled_components/Styled_Comp_exp1/src/App.js
@@ -0,0 +1,19 @@
+import React, {component} from "react";
+import {Title, TitleSmall} from './styles';
+
+function App() {
+  return (
+    <div>
+      <Title fontSize={20}>
+        Hello World
+        <span> texto menor </span>
+      </Title>
+
+      <TitleSmall>
+        Menor
+      </TitleSmall>
+    </div>
+  );
+}
+
+export default App;
